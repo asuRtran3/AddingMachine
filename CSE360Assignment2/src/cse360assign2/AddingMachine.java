e9cba4f2dbb77d5341fe707b9c84014ce497f0d2
randytran <rtran3@asu.edu>
2019-10-08 23:07:28 -0700

    Added Javadoc comments.

--- CSE360Assignment2/src/cse360assign2/AddingMachine.java
+++ CSE360Assignment2/src/cse360assign2/AddingMachine.java
@@ -3,59 +3,77 @@
  * Class ID: 1213034548
  * Assignment # 2
  * Description: An adding machine that adds and subtracts from or to the total
- * value. As well using a git and commit it to a repository to share code with others.
+ * value. As well, to practice using git and commit it to a repository to share code with others.
  */
 
 package cse360assign2;
 
 public class AddingMachine {
-    
+
     private int total;
     
-    private String operation = "0"; // To keep the history of the adding/subtracting
-    
+    private String operation = "0";
+
     public AddingMachine () {
         
         total = 0;          		// not needed - included for clarity
         
     }
     
+    /**
+     * Returns the total after addition and or subtraction
+     * @return this AddingMachine's total
+     */
     public int getTotal () {
         
-        return total;       		// simply returns the total
+        return total;
         
     }
     
+    /**
+     * This method is to add the value to the total. As well, adding
+     * the addition operator '+' and the value to the operation string to keep
+     * the history of the vale when the method is called once or multiple of times.
+     * @param value the value of the add method. 
+     */
     public void add (int value) {
     	
-    	operation += " + " + value; // This is adding to the string to keep 
-    								// each value when the method is called once 
-    								// or multiple of times.
+    	operation += " + " + value;
 
-        total += value;    			// Adding the value to the total
+        total += value;
         
     }
     
+    /**
+     * This method is to subtract the value from the total. Also, including
+     * the subtraction operator '-' and the value to store into the operation string.
+     * @param value the value of the subtract method.
+     */
     public void subtract (int value) {
     	
-        operation += " - " + value; 
+        operation += " - " + value;
 
-        total -= value;    			// Subtracting the value from the total
-        
+        total -= value;
         
     }
-   
-    
+
+    /**
+     * This is to print out the history of transactions that has been made by
+     * using the operation string. 
+     * Starts with the initial value of 0 to the last input. 
+     * Returns the private string operation variable.
+     */
     public String toString () {
-    	
-        return operation;			// Returns the operation string
-        
+
+        return operation;
+ 
     }
     
+    /**
+     * Clear method to clear the AddingMachine.
+     */
     public void clear() {
     	
-        
     }
-    
-    
+  
 }
