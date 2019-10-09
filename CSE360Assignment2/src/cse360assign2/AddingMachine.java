03620725b6abbf6f54518345f0ee8ba1dfd1f325
randytran <rtran3@asu.edu>
2019-10-08 22:23:23 -0700

    Second version. Added code to perform addition and subtraction.

--- CSE360Assignment2/src/cse360assign2/AddingMachine.java
+++ CSE360Assignment2/src/cse360assign2/AddingMachine.java
@@ -1,36 +1,61 @@
+/**
+ * Name: Randy Tran
+ * Class ID: 1213034548
+ * Assignment # 2
+ * Description: An adding machine that adds and subtracts from or to the total
+ * value. As well using a git and commit it to a repository to share code with others.
+ */
+
 package cse360assign2;
 
 public class AddingMachine {
+    
+    private int total;
+    
+    private String operation = "0"; // To keep the history of the adding/subtracting
+    
+    public AddingMachine () {
+        
+        total = 0;          		// not needed - included for clarity
+        
+    }
+    
+    public int getTotal () {
+        
+        return total;       		// simply returns the total
+        
+    }
+    
+    public void add (int value) {
+    	
+    	operation += " + " + value; // This is adding to the string to keep 
+    								// each value when the method is called once 
+    								// or multiple of times.
 
-	private int total;
-	
-	public AddingMachine () {
+        total += value;    			// Adding the value to the total
         
-		total = 0;              // not needed - included for clarity
-        
-	}
-	
-	public int getTotal () {
-        
-		return 0;
-        
-	}
-	
-	public void add (int value) {
-		
-	}
-	
-	public void subtract (int value) {
-		
-	}
-		
-	public String toString () {
-        
-		return "";
-        
-	}
+    }
+    
+    public void subtract (int value) {
+    	
+        operation += " - " + value; 
 
-	public void clear() {
-	
-	}
+        total -= value;    			// Subtracting the value from the total
+        
+        
+    }
+   
+    
+    public String toString () {
+    	
+        return operation;			// Returns the operation string
+        
+    }
+    
+    public void clear() {
+    	
+        
+    }
+    
+    
 }
